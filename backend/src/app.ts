3f28210d2719e4009258e5817b6a731e7f7ee11c
LeeUijin <n2n23339@gmail.com>
2024-04-24 14:06:28 +0900

    백엔드 환경설정, 서버 구동 코드 추가

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Express, NextFunction, Request, Response } from "express";
+import "express-async-errors";
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.sendStatus(500);
+});
+
+export { app };
