baeb643ebac5805a846085fead972d4d7b52e535
LeeUijin <n2n23339@gmail.com>
2024-05-07 17:14:58 +0900

    refactor: 프롭스를 직접 참조하여 조건부 스타일링 하도록 변경

--- frontend/src/components/common/Input.tsx
+++ frontend/src/components/common/Input.tsx
@@ -1,25 +1,24 @@
-import { EyeClosed } from '@/assets/icons/EyeClosed';
-import { Eye } from '@/assets/icons/Eye';
 import React, { ForwardedRef, useState } from 'react';
 import styled from 'styled-components';
+
+import { EyeClosed } from '@/assets/icons/EyeClosed';
+import { Eye } from '@/assets/icons/Eye';
+import Icon from './Icon';
 
 type TinputType = 'text' | 'email' | 'password' | 'number';
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   placeholder?: string;
-  inputType: TinputType;
-}
-
-interface IinputText extends React.InputHTMLAttributes<HTMLInputElement> {
-  isPassword: TinputType;
+  $inputType: TinputType;
 }
 
 const Input = React.forwardRef(
   (
-    { placeholder, inputType, onChange, ...props }: Props,
+    { placeholder, $inputType, onChange, ...props }: Props,
     ref: ForwardedRef<HTMLInputElement>,
   ) => {
-    const [inputTypeState, setInputTypeState] = useState<TinputType>(inputType);
+    const [inputTypeState, setInputTypeState] =
+      useState<TinputType>($inputType);
 
     const handleTogglePasswordType = () => {
       if (inputTypeState === 'password') {
@@ -36,10 +35,10 @@ const Input = React.forwardRef(
           ref={ref}
           type={inputTypeState}
           onChange={onChange}
-          isPassword={inputType}
+          $inputType={$inputType}
           {...props}
         />
-        {inputType === 'password' && (
+        {$inputType === 'password' && (
           <IconButton type="button" onClick={handleTogglePasswordType}>
             <Icon
               fill="#aba7af"
@@ -59,10 +58,10 @@ const InputStyle = styled.div`
   align-items: center;
 `;
 
-const InputTextStyle = styled.input<IinputText>`
+const InputTextStyle = styled.input<Props>`
   padding: 0.25rem 0.75rem;
-  padding-right: ${({ isPassword }) =>
-    isPassword === 'password' ? '42px' : '0px'};
+  padding-right: ${({ $inputType }) =>
+    $inputType === 'password' ? '42px' : '0px'};
   border: 1px solid ${({ theme }) => theme.color.border};
   border-radius: ${({ theme }) => theme.borderRadius.default};
   font-size: 1rem;
@@ -74,6 +73,8 @@ const InputTextStyle = styled.input<IinputText>`
 const IconButton = styled.button`
   position: absolute;
   right: 10px;
+  top: 50%;
+  transform: traslateY(-50%);
   top: 0;
   border: none;
   background-color: transparent;
