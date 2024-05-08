1da253302f74f8bd00d4b0bd0dbbbd5f7a77ad7c
hyemin12 <khnm31@naver.com>
2024-05-08 19:30:25 +0900

    feat: 유저 미들웨어 추가 (작성중)

---
+++ backend/src/middlewares/authentication.ts
@@ -0,0 +1,49 @@
+import { NextFunction, Response, Request } from 'express';
+import jwt from 'jsonwebtoken';
+
+import { AppDataSource } from '../data-source';
+import { User } from '../entity/user.entity';
+import { IUserInfo } from '../models/user.model';
+
+const TOKEN_PRIVATE_KEY = process.env.TOKEN_PRIVATE_KEY;
+
+export interface CustomRequest extends Request {
+  user?: IUserInfo;
+}
+
+export const authentication = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const UserRepository = AppDataSource.getRepository(User);
+  const accessToken = req.cookies['access-token'];
+
+  if (!accessToken) return res.status(401).send({ message: 'Token Not found' });
+
+  // JWT 검증
+  try {
+    jwt.verify(accessToken, TOKEN_PRIVATE_KEY!);
+  } catch (err) {
+    if (err instanceof jwt.TokenExpiredError)
+      return res.status(401).send({ message: 'SESSION_EXPIRED' });
+    return res.status(401).send({ message: 'INVALID_TOKEN' });
+  }
+
+  // JWT Decode
+  const decodedToken = jwt.decode(accessToken);
+  if (typeof decodedToken === 'string' || decodedToken === null)
+    res.status(401).send({ message: 'Invalid token payload' });
+
+  const { id } = decodedToken as IUserInfo;
+  const user = await UserRepository.findOneBy({ id: Number(id) });
+
+  if (!user) return res.status(404).send({ message: 'User not found' });
+
+  req.user = {
+    email: user.email,
+    id: user.id,
+  };
+
+  next();
+};
