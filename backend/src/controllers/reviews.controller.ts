4864bbf2969044a2a158a61e08661e29da2a1a20
LeeUijin <n2n23339@gmail.com>
2024-05-09 18:58:47 +0900

    feat: 리퀘스트 query 타입 지정

--- backend/src/controllers/reviews.controller.ts
+++ backend/src/controllers/reviews.controller.ts
@@ -1,6 +1,19 @@
 import { RequestHandler } from 'express';
 
-export const getReviews: RequestHandler = (req, res) => {
-  const re = req;
+interface IReviewQueryParams {
+  categoryId?: string;
+  isVerified?: boolean;
+  query?: string;
+  liked?: boolean;
+  best?: boolean;
+  myReviews?: boolean;
+}
+
+export const getReviews: RequestHandler<{}, {}, {}, IReviewQueryParams> = (
+  req,
+  res,
+) => {
+  const { categoryId, isVerified, query, liked, best, myReviews } = req.query;
+
   return res.status(200).json({ message: 'success' });
 };
