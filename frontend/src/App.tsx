587c64431665a2a89f9f056dce31823946ecf58f
hyemin12 <khnm31@naver.com>
2024-05-10 11:58:11 +0900

    feat: 쿼리 클라이언트 생성 및 App.tsx에 등록

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,18 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './style/theme';
 import { GlobalStyle } from './style/global';
 import { Router } from './Router';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './api/queryClient';
 
 function App() {
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Router />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Router />
+        </ThemeProvider>
+      </QueryClientProvider>
     </div>
   );
 }
