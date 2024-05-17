643e3ced9350705183503092c9811ccdba70089a
hyemin12 <khnm31@naver.com>
2024-05-17 15:16:19 +0900

    feat: 사용자 인증 미들웨어 수정

--- backend/src/middlewares/authentication.ts
+++ backend/src/middlewares/authentication.ts
@@ -15,54 +15,63 @@ export interface CustomRequest extends Request {
   user?: IUserInfo;
 }
 
-export const authentication = async (
-  req: CustomRequest,
-  res: Response,
-  next: NextFunction,
-) => {
-  const accessToken = req.cookies['access-token'];
-  if (!accessToken) {
-    throw new Error(ERROR_MESSAGE.TOKEN_NOT_FOUND);
-  }
-
-  // JWT 검증
-  try {
-    jwt.verify(accessToken, TOKEN_PRIVATE_KEY!);
-  } catch (err) {
-    if (err instanceof jwt.TokenExpiredError) {
-      throw new Error(ERROR_MESSAGE.SESSION_EXPIRED);
+/**
+ * 사용자 인증을 위한 미들웨어
+ * @param requireLogin - 로그인이 필요한지 여부를 결정 (기본값은 false)
+ * @returns Express 미들웨어 함수
+ */
+export const authentication = (requireLogin: boolean = false) => {
+  return async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const accessToken = req.cookies['access-token'];
+    if (!accessToken) {
+      if (requireLogin) {
+        return res.status(401).send({ message: ERROR_MESSAGE.TOKEN_NOT_FOUND });
+      }
+      return next();
     }
 
-    throw new Error(ERROR_MESSAGE.INVALID_TOKEN);
-  }
+    // JWT 검증
+    try {
+      jwt.verify(accessToken, TOKEN_PRIVATE_KEY!);
+    } catch (err) {
+      if (err instanceof jwt.TokenExpiredError) {
+        return res.status(401).send({ message: ERROR_MESSAGE.SESSION_EXPIRED });
+      }
+      return res.status(401).send({ message: ERROR_MESSAGE.INVALID_TOKEN });
+    }
 
-  // JWT Decode
-  const decodedToken = jwt.decode(accessToken);
-  if (typeof decodedToken === 'string' || decodedToken === null) {
-    throw new Error(ERROR_MESSAGE.INVALID_TOKEN);
-  }
+    // JWT Decode
+    const decodedToken = jwt.decode(accessToken);
+    if (typeof decodedToken === 'string' || decodedToken === null) {
+      return res.status(401).send({ message: ERROR_MESSAGE.INVALID_TOKEN });
+    }
 
-  const { email } = decodedToken as IUserInfo;
-  const userInfoResult = await getUserInfo(email);
+    const { email } = decodedToken as IUserInfo;
+    try {
+      const userInfoResult = await getUserInfo(email);
 
-  // 사용자가 차단된 경우
-  if ('suspendedUser' in userInfoResult) {
-    return res.status(403).send({
-      message: 'User is suspended',
-      ...userInfoResult.suspendedUser,
-    });
-  }
+      // 사용자가 차단된 경우
+      if ('suspendedUser' in userInfoResult) {
+        return res.status(403).send({
+          message: 'User is suspended',
+          ...userInfoResult.suspendedUser,
+        });
+      }
 
-  const { user } = userInfoResult;
-  if (!user) {
-    throw new Error(ERROR_MESSAGE.USER_NOT_FOUND);
-  }
+      const { user } = userInfoResult;
+      if (!user) {
+        return res.status(404).send({ message: ERROR_MESSAGE.USER_NOT_FOUND });
+      }
 
-  req.user = {
-    email: user.email,
-    id: user.id,
-    isAdmin: user.isAdmin,
+      req.user = {
+        email: user.email,
+        id: user.id,
+        isAdmin: user.isAdmin,
+      };
+
+      next();
+    } catch (err) {
+      next(err);
+    }
   };
-
-  next();
 };
