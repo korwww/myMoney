e26b131778ab4e340af0a333fc5f0b01f9adf003
hyemin12 <khnm31@naver.com>
2024-05-17 21:35:39 +0900

    chore: 리뷰 아이템 공유용 커밋

--- frontend/src/models/review.model.ts
+++ frontend/src/models/review.model.ts
@@ -11,12 +11,12 @@ export interface IReviewItem {
   title: string;
   createdAt: string;
   userName: string;
-  imgSrc: string;
+  reviewImg: string;
   content: string;
   verified: boolean;
   likes: number;
   userId: number;
   id: number;
-  isOwner: boolean; // 작성자 여부 (임시로 설정, api에 맞춰서 수정해야 함)
-  isLiked: boolean; // 좋아요 여부 (임시로 설정, api에 맞춰서 수정해야 함)
+  isMyReview: boolean;
+  isLiked: boolean;
 }
