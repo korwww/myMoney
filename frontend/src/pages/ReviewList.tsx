4d29d137d1f8119918837206aede4835699ffcfa
hyemin12 <khnm31@naver.com>
2024-05-19 20:08:02 +0900

    feat: 리뷰 목록 페이지 카테고리 적용

--- frontend/src/pages/ReviewList.tsx
+++ frontend/src/pages/ReviewList.tsx
@@ -1,13 +1,9 @@
-import { useSearchParams } from 'react-router-dom';
-
 import ReviewList from '@/components/common/ReviewList';
-import { QUERYSTRING } from '@/constance/querystring';
 import { useReviews } from '@/hooks/useReviews';
 import Layout from '@/layout/Layout';
+import Category from '@/components/common/Category';
 
 function ReviewListPage() {
-  const [searchParams, setSearchParams] = useSearchParams();
-
   const {
     reviews,
     isLoadingFetchReviews,
@@ -15,34 +11,9 @@ function ReviewListPage() {
     hasNextPageFetchReviews,
   } = useReviews();
 
-  const handleCategoryIdParams = (id: number) => {
-    const newSearchParams = new URLSearchParams(searchParams);
-
-    if (!id) {
-      newSearchParams.delete(QUERYSTRING.CATEGORY_ID);
-    } else {
-      newSearchParams.set(QUERYSTRING.CATEGORY_ID, id.toString());
-    }
-
-    newSearchParams.delete(QUERYSTRING.IS_VERIFIED);
-    setSearchParams(newSearchParams);
-  };
-
-  const handleIsVerifiedParams = () => {
-    const newSearchParams = new URLSearchParams(searchParams);
-
-    if (newSearchParams.get(QUERYSTRING.IS_VERIFIED)) {
-      newSearchParams.delete(QUERYSTRING.IS_VERIFIED);
-    } else {
-      newSearchParams.set(QUERYSTRING.IS_VERIFIED, 'true');
-    }
-
-    newSearchParams.delete(QUERYSTRING.CATEGORY_ID);
-    setSearchParams(newSearchParams);
-  };
-
   return (
     <Layout title="리뷰 목록" showBackButton={false}>
+      <Category />
       <ReviewList
         reviews={reviews}
         isLoading={isLoadingFetchReviews}
