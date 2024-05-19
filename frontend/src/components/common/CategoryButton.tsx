70372b518e090de23f50575533c4b540e9b8be62
LeeUijin <n2n23339@gmail.com>
2024-05-19 10:22:50 +0900

    feat: 카테고리 버튼의 카테고리들을 임의로 정한 객체로 바꾸고 객체를 요소들에게 매핑, 링크 추가

--- frontend/src/components/common/CategoryButton.tsx
+++ frontend/src/components/common/CategoryButton.tsx
@@ -14,32 +14,72 @@ import { SealCheck } from '@/assets/icons/SealCheck';
 import { Link } from 'react-router-dom';
 
 const categorys = [
-  <Devices />,
-  <TShirt />,
-  <Armchair />,
-  <Oven />,
-  <FilmSlate />,
-  <Hamburger />,
-  <HairDryer />,
-  <MapPin />,
-  <DotsThree />,
-  <SealCheck />,
+  {
+    categoryId: 1,
+    categoryName: '디지털',
+    element: <Devices />,
+  },
+  {
+    categoryId: 2,
+    categoryName: '의류',
+    element: <TShirt />,
+  },
+  {
+    categoryId: 3,
+    categoryName: '가구/인테리어',
+    element: <Armchair />,
+  },
+  {
+    categoryId: 4,
+    categoryName: '가전',
+    element: <Oven />,
+  },
+  {
+    categoryId: 5,
+    categoryName: '문화',
+    element: <FilmSlate />,
+  },
+  {
+    categoryId: 6,
+    categoryName: '식품',
+    element: <Hamburger />,
+  },
+  {
+    categoryId: 7,
+    categoryName: '뷰티/미용',
+    element: <HairDryer />,
+  },
+  {
+    categoryId: 8,
+    categoryName: '장소',
+    element: <MapPin />,
+  },
+  {
+    categoryId: 9,
+    categoryName: '기타',
+    element: <DotsThree />,
+  },
+  {
+    categoryId: 10,
+    categoryName: '인증',
+    element: <SealCheck />,
+  },
 ];
 
 function CateogoryButton() {
   return (
     <CateogoryButtonStyle>
       <div className="items">
-        {categorys.map((category, idx) => (
+        {categorys.map((category) => (
           <div className="item">
-            <StyledLink to={''}>
+            <StyledLink to={`/reviews?categoryId=${category.categoryId}`}>
               <Button
-                key={idx}
-                children={category}
+                key={category.categoryId}
+                children={category.element}
                 size={'small'}
                 scheme={'disabled'}
               />
-              <p>카테고리</p>
+              <p>{category.categoryName}</p>
             </StyledLink>
           </div>
         ))}
