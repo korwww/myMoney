8afb1b9ada2be4d465e5659fc3c9ff238415796e
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2024-05-20 00:54:51 +0900

    feat: 검색페이지 기능구현, 큰 돋보기 아이콘 추가 및 리뷰 api파일, hook 파일 수정

--- frontend/src/assets/icons/BigSearch.tsx
+++ frontend/src/assets/icons/BigSearch.tsx
@@ -1,4 +1,4 @@
-export function Search() {
+export function BigSearch() {
   return (
     <svg
       width="167"
--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,49 +1,152 @@
 import Layout from '@/layout/Layout';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
-import { useMutation } from '@tanstack/react-query';
-import { IsearchQuery, postSearchResults } from '@/api/search.api';
 import Input from '@/components/common/Input';
 import { MagnifyingGlass } from '@/assets/icons/MagnifyingGlass';
+import { useEffect, useRef, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { useReviews } from '@/hooks/useReviews';
+import { SmallX } from '@/assets/icons/SmallX';
+import { BigSearch } from '@/assets/icons/BigSearch';
+import ReviewList from '@/components/common/ReviewList';
+
+interface Ikey {
+  id: number;
+  text: string;
+}
 
 function Search() {
-  const { register, handleSubmit } = useForm<IsearchQuery>();
-  const onSubmit = (data: IsearchQuery) => {
-    SearchMutation.mutate(data);
+  const [searchParams, setSearchParams] = useSearchParams();
+  // 로컬 스토리지에 저장한 검색어를 관리할 useState keywords
+  const [keywords, setKeywords] = useState<Ikey[]>([]);
+  const { reviews, isLoadingFetchReviews, fetchReviewsNextPage } = useReviews();
+  const { register, handleSubmit, setValue } = useForm<{ query: string }>();
+  const onSubmit = (data: { query: string }) => {
+    setSearchParams({ query: data.query });
+    const newKeyword = {
+      id: Date.now(),
+      text: data.query,
+    };
+    const updatedKeywords = [
+      newKeyword,
+      ...keywords.filter((keyword) => keyword.text !== data.query),
+    ];
+    setKeywords(updatedKeywords.slice(0, 5));
   };
 
-  const SearchMutation = useMutation({
-    mutationFn: postSearchResults,
-    onSuccess(res) {
-      console.log('검색 성공!');
-    },
-    onError(err) {
-      console.log('검색 실패!');
-    },
-  });
+  // ① window 즉, 브라우저가 모두 렌더링된 상태에서 해당 함수를 실행할 수 있도록 작업
+  useEffect(() => {
+    const result = localStorage.getItem('keywords') || '[]';
+    setKeywords(JSON.parse(result));
+  }, []);
+
+  // ② keywords 객체를 의존하여, 변경될 경우 새롭게 localStroage의 아이템 'keywords'를 세팅한다
+  useEffect(() => {
+    localStorage.setItem('keywords', JSON.stringify(keywords));
+  }, [keywords]);
+
+  const buttonRef = useRef<null | HTMLButtonElement>(null);
+
+  const handleRemoveKeyword = (id: number) => {
+    const nextKeyword = keywords.filter((keyword) => {
+      return keyword.id != id;
+    });
+    setKeywords(nextKeyword);
+  };
 
   return (
-    <Layout showBackButton={true} title="검색">
+    <Layout showBackButton={false} title="검색">
       <Container>
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <Fieldset>
-            <SearchIcon onClick={() => alert('아이콘 클릭!')}>
-              <MagnifyingGlass />
-            </SearchIcon>
-            <StyledInput
-              $inputType="text"
-              {...register('query', { required: true })}
-              type="text"
-              placeholder="검색어를 입력해주세요"
-            />
-          </Fieldset>
-        </Form>
+        <StickyContainer>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Fieldset>
+              <Button type="submit" ref={buttonRef} />
+              <SearchIcon
+                onClick={() => {
+                  if (buttonRef.current === null) {
+                    return;
+                  } else {
+                    buttonRef.current.click();
+                  }
+                }}
+              >
+                <MagnifyingGlass />
+              </SearchIcon>
+              <CancleIcon onClick={() => setValue('query', '')}>
+                <SmallX />
+              </CancleIcon>
+              <StyledInput
+                defaultValue={String(searchParams.get('query') || '')}
+                $inputType="text"
+                {...register('query', { required: true })}
+                type="text"
+                placeholder="검색어를 입력해주세요"
+              />
+            </Fieldset>
+          </Form>
+        </StickyContainer>
+        {keywords.length === 0 ? (
+          <NoRecentSearchResult>
+            <BigSearch />
+            <p>최근검색어가 없습니다.</p>
+          </NoRecentSearchResult>
+        ) : (
+          <RecentKeywordContainer>
+            <h2>최근검색어</h2>
+            {keywords.map((keyword) => (
+              <RecentKeywordInnerContainer key={keyword.id}>
+                <div>{keyword.text}</div>
+                <div onClick={() => handleRemoveKeyword(keyword.id)}>
+                  <SmallX />
+                </div>
+              </RecentKeywordInnerContainer>
+            ))}
+          </RecentKeywordContainer>
+        )}
+        <SearchResultContainer>
+          <ReviewList
+            title={'최신글'}
+            reviews={reviews}
+            isLoading={false}
+            text={'찾으시는 검색결과가 없습니다.'}
+          />
+        </SearchResultContainer>
       </Container>
     </Layout>
   );
 }
 
 const Container = styled.div``;
+
+const SearchResultContainer = styled.div``;
+
+const RecentKeywordContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  padding: 15px;
+  h2 {
+    font-weight: ${({ theme }) => theme.fontWeight.bold};
+  }
+`;
+
+const RecentKeywordInnerContainer = styled.div`
+  padding: 19px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 20px;
+`;
+
+const NoRecentSearchResult = styled.div`
+  padding-top: 25vh;
+  text-align: center;
+`;
+
+const StickyContainer = styled.div`
+  position: sticky;
+  top: 0;
+`;
 
 const Form = styled.form``;
 
@@ -59,17 +162,45 @@ const StyledInput = styled(Input)`
   width: 100%;
 `;
 
+const Button = styled.button`
+  display: none;
+`;
+
 const SearchIcon = styled.div`
   position: absolute;
   z-index: 2;
   top: 50%;
-  left: 10px; /* 인풋 필드 내부 왼쪽으로 정확한 위치 조정 */
+  left: 10px;
   transform: translateY(-50%);
   cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 40px; /* 인풋 필드 높이와 일치 */
+  height: 40px;
+`;
+
+const CancleIcon = styled.div`
+  fill: ${({ theme }) => theme.buttonScheme.disabled};
+  position: absolute;
+  z-index: 2;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
 `;
 
 export default Search;
+
+/**
+ * 추가 구현사항 목록
+ * 1. 저장된 검색어 화면에 표시. -> 최근 검색어 표시. ✅
+ * 2. 최근검색어를 지우는 기능.(X 아이콘 사용)✅
+ * 3. 최근검색어 저장 개수 limit제한 (오래된 순서부터)✅
+ * 4. useReviews 이용해서 검색결과 화면에 표시하는 것.
+ * 5. 검색결과가 없는 경우 & 최근검색결과가 없는 경우✅의 화면 구현.
+ * 6. 검색 취소하는것(검색창 초기화) -> x 아이콘 추가해서 기능 구현. ✅
+ */
