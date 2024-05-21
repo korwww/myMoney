895f86f20dad87cb5c6bdb36269aab0ff8eec14b
LeeUijin <n2n23339@gmail.com>
2024-05-22 03:13:32 +0900

    chore: 배포를 위한 설정 변경

--- backend/src/settings.ts
+++ backend/src/settings.ts
@@ -4,12 +4,13 @@ dotenv.config();
 
 export const CORS_ALLOWED_ORIGIN = process.env.CORS_ALLOWED_ORIGIN;
 export const PORT = parseInt(process.env.PORT || '3031');
-export const DB_HOST = process.env.DB_HOST;
+export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PORT = parseInt(process.env.DB_PORT || '3306');
-export const DB_DATABASE = process.env.DB_DATABASE;
-export const DB_USERNAME = process.env.DB_USERNAME;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_DATABASE = process.env.DB_DATABASE || 'MyMoney';
+export const DB_USERNAME = process.env.DB_USERNAME || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'root';
 export const DB_LOGGING: LoggerOptions | undefined =
   (process.env.DB_LOGGING as LoggerOptions) || false;
 export const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS!, 10) || 10;
-export const TOKEN_PRIVATE_KEY = process.env.TOKEN_PRIVATE_KEY;
+export const TOKEN_PRIVATE_KEY =
+  process.env.TOKEN_PRIVATE_KEY || 'NaeDonNaeSan';
