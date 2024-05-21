9b26dab8f19e3fc36527a27e73c0cd7de38ab130
JiwonYun <85991535+yuunstory@users.noreply.github.com>
2024-05-21 17:12:07 +0900

    style: 메인페이지에서 베스트 리뷰 이미지 가로 기준으로 화면에 꽉차게 수정

--- frontend/src/components/Home/BestReview.tsx
+++ frontend/src/components/Home/BestReview.tsx
@@ -71,9 +71,11 @@ const BestReviewStyle = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    height: 100%;
+    width: 100%;
   }
   .img {
-    height: 350px;
+    width: 100%;
     object-fit: cover;
   }
 
